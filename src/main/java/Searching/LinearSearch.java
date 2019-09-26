13b8e5661f759a04c8f11570126a289babe1b164
Arunkumar C <arunkumar_c@external.mckinsey.com>
2019-09-26 12:26:55 +0530

    Updating linear search code

--- src/main/java/Searching/LinearSearch.java
+++ src/main/java/Searching/LinearSearch.java
@@ -1,13 +1,10 @@
 package Searching;
 
 public class LinearSearch {
-
-    public static void main(String[] args) {
-        int []data={56,3,5,87,90,76};
-        int searchEle=76;
+    public  static void searchEle(int searchele,int []data){
         int i=0;
         for(i=0;i<data.length;i++){
-            if(data[i]==searchEle){
+            if(data[i]==searchele){
                 System.out.println("Find the element in the place "+(i+1));
                 break;
             }
@@ -15,5 +12,11 @@ public class LinearSearch {
         if(i==data.length){
             System.out.println("Element is not available in the given data");
         }
+    }
+    public static void main(String[] args) {
+        int []data={56,3,5,87,90,76};
+        int searchEle=76;
+        searchEle(searchEle,data);
+
     }
 }
