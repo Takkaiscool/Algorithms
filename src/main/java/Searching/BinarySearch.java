a5004f2c30d91e47964aa0bc40d7df97e0d105b3
Arunkumar C <arunkumar_c@external.mckinsey.com>
2019-09-26 12:41:27 +0530

    Added binary search with recursive function also

--- src/main/java/Searching/BinarySearch.java
+++ src/main/java/Searching/BinarySearch.java
@@ -1,12 +1,12 @@
 package Searching;
 
 public class BinarySearch {
-    public static int binarySearch(int []data,int first,int last,int key){
+    public static int binarySearch(int []data,int first,int last,int searchElement){
         int mid=(first+last)/2;
         while (first<=last){
-            if(data[mid]<key){
+            if(data[mid]<searchElement){
                 first=mid+1;
-            }else if(data[mid]==key){
+            }else if(data[mid]==searchElement){
                 return mid+1;
             }else{
                 last=mid-1;
@@ -15,11 +15,26 @@ public class BinarySearch {
         }
         return 0;
     }
+
+    public static int binarySearchWithRecursive(int []data,int first,int last,int searchElement){
+        int mid=(first+last)/2;
+
+            if (data[mid] == searchElement)
+                return mid + 1;
+            else if (data[mid] < searchElement)
+               return  binarySearch(data, mid + 1, last, searchElement);
+            else if (data[mid] > searchElement)
+                return binarySearch(data, first, mid - 1, searchElement);
+
+        return 0;
+
+    }
     public static void main(String[] args) {
         int data[] = {10,20,30,40,50};
-        int key = 60;
+        int key = 50;
         int last=data.length-1;
         System.out.println(binarySearch(data,0,last,key));
+        System.out.println(binarySearchWithRecursive(data,0,last,key));
 
 
     }
